43c589bc960628f22d528471dba0e83258fd42d3
Juan <juan.jmg95@gmail.com>
2020-05-20 22:48:38 -0300

    primer commit

---
+++ clientUDP.py
@@ -0,0 +1,30 @@
+import socket
+import select
+import time
+
+UDP_IP = 'localhost'
+IN_PORT = 5005
+timeout = 3
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, IN_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    time.sleep(10.5)
+    if data:
+        print "File name:", data
+        file_name = data.strip()
+
+    f = open(file_name, 'wb')
+
+    while True:
+        ready = select.select([sock], [], [], timeout)
+        if ready[0]:
+            data, addr = sock.recvfrom(1024)
+            f.write(data)
+        else:
+            print "%s Finish!" % file_name
+            f.close()
+            break
